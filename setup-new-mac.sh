e46fc3b19a52cb4650f70dced27212fdc5c8aaac
Li Mengyang <lmy86263@gmail.com>
2022-07-03 12:18:16 +0800

    Create setup-new-mac.sh

---
+++ setup-new-mac.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Shell Programs
+brew install \
+autojump \ 
+z \ 
+bat \ 
+zsh \ 
+tldr \ 
+curl \ 
+tree \ 
+exa \ 
+proxychains-ng \ 
+wget \ 
+fzf && 
+
+# GUI Programs
+brew install --cask \ 
+bob \ 
+hiddenbar \ 
+rectangle \ 
+brave-browser \ 
+iina \ 
+shottr \ 
+caffeine \ 
+iterm2 \ 
+sogouinput \ 
+coconutbattery \ 
+itsycal \ 
+stats \ 
+fig \ 
+keka \ 
+visual-studio-code \ 
+flux \ 
+lulu \ 
+wechat \ 
+github \ 
+neteasemusic \ 
+zotero \ 
+google-chrome \ 
+notion \ 
+google-drive \ 
+postman \ 
+tencent-lemon \ 
